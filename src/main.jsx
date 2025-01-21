65f9035970e09999511db6e7705d6fe70cba02c0
Issac038 <issac.antony@kalvium.community>
2025-01-21 09:44:49 +0530

    added card component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Usercard from './cart/usercard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Usercard/>
   </StrictMode>,
 )
